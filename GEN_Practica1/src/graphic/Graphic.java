a3f37a3d5a4dc8b242b0ec4e9398c21f2dee196e
rafalonsog <60778820+rafalonsog@users.noreply.github.com>
2023-02-10 15:04:13 +0100

    Delete GEN_Practica1 directory

--- GEN_Practica1/src/graphic/Graphic.java
+++
@@ -1,8 +0,0 @@
-package graphic;
-
-public class Graphic {
-
-	/* Normalmente se pinta el azul rl máximo absoluto (mejor valor intergeneracional, en rojo el máximo relativo (mejor valor de esa generación),
-	 * la pesión selectiva (en amarillo), el elitismo (vere?)
-	 */
-}
--- GEN_Practica1/src/model/fitness/Fitness.java
+++
@@ -1,6 +0,0 @@
-package model.fitness;
-
-public interface Fitness {
-
-	Double getValue();
-}
