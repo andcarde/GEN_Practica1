47ffa7853bc82351b4e923b74be7a63ff047d535
rafalonsog <rafas1367@gmail.com>
2023-02-22 10:24:31 +0100

    small fix main

--- G13P01/src/launcher/Main.java
+++ G13P01/src/launcher/Main.java
@@ -1,11 +1,12 @@
 package launcher;
 
+import graphic.Controller;
 import graphic.Window;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Window window = new Window();
+		Window window = new Window(Controller.getInstance());
 		window.setVisible(true);
 	}
 }
