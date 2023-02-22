99f90d292af7b1613545ba58ddebeaff9ecfb92f
Andrés Cardenal Antón <andrescardenalanton@gmail.com>
2023-02-22 14:02:24 +0100

    Cambios en el método de Torneo

--- G13P01/src/model/selection/DeterministicTournament.java
+++ G13P01/src/model/selection/DeterministicTournament.java
@@ -15,12 +15,26 @@ public class DeterministicTournament extends Tournament {
 	@Override
 	public List<ChromosomeI> act(List<ChromosomeI> population) {
 		List<ChromosomeI> selected = new ArrayList<>();
+		if (this.contestantsAmount > 0) {
 		List<ChromosomeI> populationCopy = List.copyOf(population);
 		List<ChromosomeI> match;
 		for (int i = 0; i < population.size(); i++) {
 			match = new ArrayList<>();
-			for (int j = 0; j < contestantsAmount; j++)
-				match.add(populationCopy.get(RandomGenerator.createAleatoryInt(populationCopy.size())));
+			for (int j = 0; j < contestantsAmount; j++) {
+				int randomIndex = RandomGenerator.createAleatoryInt(populationCopy.size());
+				match.add(populationCopy.get(randomIndex));
+				populationCopy.remove(randomIndex);
+			}
+			ChromosomeI winner = match.get(0);
+			for (int j = 1; i < contestantsAmount; i++) {
+				ChromosomeI actual = match.get(j);
+				if (winner.getValue() < actual.getValue())
+					winner = actual;
+			}
+			if (!selected.contains(winner))
+				selected.add(winner);
+			else
+				selected.add(winner.copy());
 			for (ChromosomeI ch : match)
 				populationCopy.add(ch);
 		}
--- G13P01/src/model/selection/ProbabilisticTournament.java
+++ G13P01/src/model/selection/ProbabilisticTournament.java
@@ -1,8 +1,10 @@
 package model.selection;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import model.chromosome.ChromosomeI;
+import model.random.RandomGenerator;
 
 public class ProbabilisticTournament extends Tournament {
 
@@ -15,7 +17,16 @@ public class ProbabilisticTournament extends Tournament {
 
 	@Override
 	public List<ChromosomeI> act(List<ChromosomeI> population) {
-		// TODO Falta por implementar
-		return null;
+		List<ChromosomeI> selected = new ArrayList<>();
+		List<ChromosomeI> populationCopy = List.copyOf(population);
+		List<ChromosomeI> match;
+		for (int i = 0; i < population.size(); i++) {
+			match = new ArrayList<>();
+			for (int j = 0; j < contestantsAmount; j++)
+				match.add(populationCopy.get(RandomGenerator.createAleatoryInt(populationCopy.size())));
+			for (ChromosomeI ch : match)
+				populationCopy.add(ch);
+		}
+		return selected;
 	}
 }
