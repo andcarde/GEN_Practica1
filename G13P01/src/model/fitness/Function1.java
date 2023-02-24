3c31edb8fefa41610eb5d3cb5033c8cf29d4cb35
rafalonsog <rafas1367@gmail.com>
2023-02-24 14:20:51 +0100

    Fix Function1 y Table

--- G13P01/src/model/fitness/Function1.java
+++ G13P01/src/model/fitness/Function1.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class Function1 extends Function {
 
-	/* La primera función se llama de calibración porque es simple y
-	 * por lo tanto debe converger de forma más simple.
+	/* La primera funciï¿½n se llama de calibraciï¿½n porque es simple y
+	 * por lo tanto debe converger de forma mï¿½s simple.
 	 */
 	
 	public Function1(Double precision) {
@@ -25,6 +25,7 @@ public class Function1 extends Function {
 	public List<Variable> getVariables() {
 		List<Variable> variables = new ArrayList<>();
 		variables.add(new Variable("x1", -3.0, 12.1, this.precision));
+		variables.add(new Variable("x2", 4.1, 5.8, this.precision));
 		return variables;
 	}
 }
