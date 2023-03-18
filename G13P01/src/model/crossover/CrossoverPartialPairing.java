79f4f528a02024a33aaaedbac9bbbbd6aec39a49
rafalonsog <rafas1367@gmail.com>
2023-03-18 11:51:33 +0100

    Fix CrossoverPartialPairing

--- G13P01/src/model/crossover/CrossoverPartialPairing.java
+++ G13P01/src/model/crossover/CrossoverPartialPairing.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import model.MoldI;
@@ -19,15 +20,8 @@ public class CrossoverPartialPairing extends Crossover {
 
 	@Override
 	public List<ChromosomeI> cross(ChromosomeI parent1, ChromosomeI parent2) {
-		HashMap<Integer, Integer> p1 = new HashMap<>(), p2 = new HashMap<>(), s1 = new HashMap<>(), s2 = new HashMap<>();
-		List<Integer> s1_missing_indexes= new ArrayList<>(), s2_missing_indexes = new ArrayList<>(), p1_missing_indexes = new ArrayList<>(), p2_missing_indexes= new ArrayList<>(),
-		s1_not_added = new ArrayList<>(), s2_not_added = new ArrayList<>(), p1_not_added = new ArrayList<>(), p2_not_added = new ArrayList<>();
-		for (int i = 0; i < parent1.getSize(); i++) {
-			s1_missing_indexes.add(i);
-			s2_missing_indexes.add(i);
-			p1_missing_indexes.add(i);
-			p2_missing_indexes.add(i);
-		}
+		HashMap<Integer, Integer> p1 = new HashMap<>(), p2 = new HashMap<>(), s1 = new HashMap<>(), s2 = new HashMap<>(), p1_not_added = new HashMap<>(), p2_not_added = new HashMap<>();;
+		List<Integer> s1_not_added = new ArrayList<>(), s2_not_added = new ArrayList<>(); 
 		int lower_bound = RandomGenerator.createAleatoryInt(parent1.getSize());
 		int upper_bound = RandomGenerator.createAleatoryInt(parent1.getSize());
 		while (lower_bound == upper_bound) {
@@ -38,78 +32,87 @@ public class CrossoverPartialPairing extends Crossover {
 			upper_bound = lower_bound;
 			lower_bound = tmp;
 		}
+		
+		//Introducimos los genes del intervalo en el mapa
 		for (int i = 0; i < parent1.getSize(); i++) {
 			
 			p1.put(i, Converter.DoubleToInt((Double) parent1.getGen(i).getGenoma()));
 			p2.put(i, Converter.DoubleToInt((Double) parent2.getGen(i).getGenoma()));
 			if (i >= lower_bound && i <= upper_bound) {
-				s1_missing_indexes.remove((Object) i);
-				s2_missing_indexes.remove((Object) i);
-				p1_missing_indexes.remove((Object) i);
-				p2_missing_indexes.remove((Object) i);
-				continue;
+				s1.put(i,  Converter.DoubleToInt((Double) parent2.getGen(i).getGenoma()));
+				s2.put(i,  Converter.DoubleToInt((Double) parent1.getGen(i).getGenoma()));
+				
 			}
-			s1.put(i,  Converter.DoubleToInt((Double) parent2.getGen(i).getGenoma()));
-			s2.put(i,  Converter.DoubleToInt((Double) parent1.getGen(i).getGenoma()));
 		}
 		
 		ChromosomeI son1 = parent2.copy();
 		ChromosomeI son2 = parent1.copy();
 		
+		//introducimos los genes validos con su respectivo padre
 		for (int i = 0; i < parent1.getSize(); i++) {
 			if (i >= lower_bound && i <= upper_bound) continue;
-			if (!s1.containsValue(Converter.DoubleToInt((Double) parent1.getGen(i).getGenoma()))) {
-				s1.put(i,  Converter.DoubleToInt((Double) parent1.getGen(i).getGenoma()));
+			int value = Converter.DoubleToInt((Double) parent1.getGen(i).getGenoma());
+			if (!s1.containsValue(value)) {
+				s1.put(i, value);
 				son1.setGen(i, parent1.getGen(i));
-				s1_missing_indexes.remove((Object) i);
-				p1_missing_indexes.remove((Object) i);
+				
 			}
 			else {
 				s1_not_added.add(i);
-				p1_not_added.add(i);
 			}
-			if (!s2.containsValue(Converter.DoubleToInt((Double) parent2.getGen(i).getGenoma()))) {
-				s2.put(i,  Converter.DoubleToInt((Double) parent2.getGen(i).getGenoma()));
+			value = Converter.DoubleToInt((Double) parent2.getGen(i).getGenoma());
+			if (!s2.containsValue(value)) {
+				s2.put(i, value);
 				son2.setGen(i, parent2.getGen(i));
-				s2_missing_indexes.remove((Object) i);
-				p2_missing_indexes.remove((Object) i);
+				
 			}
 			else {
 				s2_not_added.add(i);
-				p2_not_added.add(i);
 			}
 		}
-		Set<Object> aux1 = new HashSet<>(), aux2 = new HashSet<>();
 		
-		
-		while (!s1_not_added.isEmpty()) {
-			int son_ind = s1_not_added.get(0), par_ind = p1_not_added.get(0);
-			son1.setGen(son_ind, parent1.getGen(par_ind));
-			s1_not_added.remove((Object) son_ind);
-			p1_not_added.remove((Object) par_ind);
-		}
-		while (!s2_not_added.isEmpty()) {
-			int son_ind = s2_not_added.get(0), par_ind = p2_not_added.get(0);
-			son2.setGen(son_ind, parent2.getGen(par_ind));
-			s2_not_added.remove((Object) son_ind);
-			p2_not_added.remove((Object) par_ind);
+		//Buscamos los genes restantes
+		for (int i = 0; i < parent1.getSize(); i++) {
+			int value = Converter.DoubleToInt((Double) parent1.getGen(i).getGenoma());
+			if (!s1.containsValue(value)) {
+				p1_not_added.put(i, value);
+			}
+			value = Converter.DoubleToInt((Double) parent2.getGen(i).getGenoma());
+			if (!s2.containsValue(value)) {
+				p2_not_added.put(i, value);
+			}
 		}
 		
+		//Metemos los genes restantes
+		int count = 0;
+		for (Entry<Integer, Integer> set : p1_not_added.entrySet()) {
+			if (s1.containsValue(set.getValue())) {
+				System.out.println(set.getValue());
+			}
+			s1.put(s1_not_added.get(count), set.getValue());
+			count++;
+		}
+		count = 0;
+		for (Entry<Integer, Integer> set : p2_not_added.entrySet()) {
+			if (s2.containsValue(set.getValue())) {
+				System.out.println(set.getValue());
+			}
+			s2.put(s2_not_added.get(count), set.getValue());
+			count++;
+		}
+		
+		//Copiamos el hashmap a los cromosomas
+		for (int i = 0; i < s1.size(); i++) {
+			son1.getGen(i).assimilate(s1.get(i));
+		}
+		for (int i = 0; i < s2.size(); i++) {
+			son2.getGen(i).assimilate(s2.get(i));
+		}
+
 		List<ChromosomeI> sons = new ArrayList<>();
 		sons.add(son1); sons.add(son2);
-		aux1 = new HashSet<>(); aux2 = new HashSet<>();
-		for (int i = 0; i < son1.getGenes().size(); i++) {
-			if (aux1.contains(son1.getGen(i).getGenoma())) {
-				System.out.println(son1.getGen(i).getGenoma());
-			}
-			else aux1.add(son1.getGen(i).getGenoma());
-		}
-		for (int i = 0; i < son2.getGenes().size(); i++) {
-			if (aux2.contains(son2.getGen(i).getGenoma())) {
-				System.out.println(son2.getGen(i).getGenoma());
-			}
-			else aux2.add(son2.getGen(i).getGenoma());
-		}
+
+	
 		return sons;
 	}
 
