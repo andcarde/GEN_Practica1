3e34b479abf147261a31462553974c9bc756f796
Andrés Cardenal Antón <andrescardenalanton@gmail.com>
2023-03-29 18:13:23 +0200

    Comienzo de arreglo de la Mutación Eugenésica
    - Quedan errores de ejecución por solucionar.

--- G13P02/src/model/mutation/CityEugenicMutation.java
+++ G13P02/src/model/mutation/CityEugenicMutation.java
@@ -7,7 +7,7 @@ import model.chromosome.ChromosomeI;
 import model.chromosome.GenI;
 import model.fitness.FunctionTSP;
 import model.random.RandomGenerator;
-import model.util.Pair;
+import model.util.Converter;
 
 public class CityEugenicMutation implements CityMutationI {
 	
@@ -25,26 +25,39 @@ public class CityEugenicMutation implements CityMutationI {
 		
 		// Sacamos el valor de cada ciudad, el cual se obtiene al hallar 
 		// la distancia de sus adyacentes
-		List<Pair<String, Double>> valores = new ArrayList<>();
-		for (int i = 0; i < genes.size(); i++) { 
-			int izq = i - 1; int der = i + 1;
-			if (i == 0) izq = genes.size() - 1;
-			if (der == genes.size() - 1) izq = 0;
-			Double value = (FunctionTSP.getDist(i, izq) + FunctionTSP.getDist(i, der))/2 + 0.0; 
-			valores.add(new Pair<String, Double>(genes.get(i).getName(), value));
+		
+		List<Double> valores = new ArrayList<>();
+		int act, der, izq;
+		Double value;
+		
+		act = Converter.DoubleToInt(genes.get(0).getValue());
+		der = Converter.DoubleToInt(genes.get(1).getValue());
+		value = (FunctionTSP.getDist(act, 25) + FunctionTSP.getDist(act, der)) / 2 + 0.0;
+		valores.add(value);
+		
+		for (int i = 1; i < genes.size(); i++) {
+			act = Converter.DoubleToInt(genes.get(i).getValue());
+			izq = Converter.DoubleToInt(genes.get(i - 1).getValue());
+			der = Converter.DoubleToInt(genes.get(i + 1).getValue());
+			value = (FunctionTSP.getDist(act, izq) + FunctionTSP.getDist(act, der)) / 2 + 0.0; 
+			valores.add(value);
 		}
+		
+		act = Converter.DoubleToInt(genes.get(genes.size() - 1).getValue());
+		izq = Converter.DoubleToInt(genes.get(genes.size() - 2).getValue());
+		value = (FunctionTSP.getDist(act, izq) + FunctionTSP.getDist(act, 25)) / 2 + 0.0; 
+		valores.add(value);
 		
 		// Hallamos los dos peores indices
 		int worst_index = 0;
 		int less_worse_index = 1;
-		if (valores.get(worst_index).getR() > valores.get(less_worse_index).getR()) {
-			int tmp = worst_index;
-			worst_index = less_worse_index;
-			less_worse_index = tmp;
+		if (valores.get(less_worse_index) > valores.get(worst_index)) {
+			worst_index = 1;
+			less_worse_index = 0;
 		} 
 		for (int i = 2; i < valores.size(); i++) {
-			if (valores.get(i).getR() < valores.get(less_worse_index).getR()) {
-				if (valores.get(i).getR() < valores.get(worst_index).getR()) {
+			if (valores.get(i) < valores.get(less_worse_index)) {
+				if (valores.get(i) < valores.get(worst_index)) {
 					less_worse_index = worst_index;
 					worst_index = i;
 				}
@@ -55,11 +68,10 @@ public class CityEugenicMutation implements CityMutationI {
 		// Los intercambiamos de posicion
 		GenI worst = genes.get(worst_index).copy();
 		GenI less_worse = genes.get(less_worse_index).copy();
-		String worst_name = worst.getName();
-		worst.setName(less_worse.getName());
-		less_worse.setName(worst_name);
-		genes.set(less_worse_index, worst);
-		genes.set(worst_index, less_worse);
+		Double aux = worst.getValue();
+		worst.assimilate(less_worse.getValue());
+		less_worse.assimilate(aux);
+		
 		return genes;
 	}
 }
