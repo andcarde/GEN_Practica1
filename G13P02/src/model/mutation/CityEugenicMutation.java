0fcd26e0084cef965a6ce5adb58ac4dfa8ee2daf
Andrés Cardenal Antón <andrescardenalanton@gmail.com>
2023-03-29 18:28:21 +0200

    Mutación Eugenésica completamente arreglado y funcional

--- G13P02/src/model/mutation/CityEugenicMutation.java
+++ G13P02/src/model/mutation/CityEugenicMutation.java
@@ -23,19 +23,22 @@ public class CityEugenicMutation implements CityMutationI {
 		if (!RandomGenerator.createAleatoryBoolean(mutationProbability))
 			return genes;
 		
-		// Sacamos el valor de cada ciudad, el cual se obtiene al hallar 
-		// la distancia de sus adyacentes
+		/* Sacamos el valor de cada ciudad, el cual se obtiene al hallar 
+		 * la media de la distancia entre esta y sus adyacentes
+		 */
 		
 		List<Double> valores = new ArrayList<>();
 		int act, der, izq;
 		Double value;
 		
+		// Primer valor
 		act = Converter.DoubleToInt(genes.get(0).getValue());
 		der = Converter.DoubleToInt(genes.get(1).getValue());
 		value = (FunctionTSP.getDist(act, 25) + FunctionTSP.getDist(act, der)) / 2 + 0.0;
 		valores.add(value);
 		
-		for (int i = 1; i < genes.size(); i++) {
+		// Valores intermedios
+		for (int i = 1; i < genes.size() - 1; i++) {
 			act = Converter.DoubleToInt(genes.get(i).getValue());
 			izq = Converter.DoubleToInt(genes.get(i - 1).getValue());
 			der = Converter.DoubleToInt(genes.get(i + 1).getValue());
@@ -43,21 +46,22 @@ public class CityEugenicMutation implements CityMutationI {
 			valores.add(value);
 		}
 		
+		// Últtimo valor
 		act = Converter.DoubleToInt(genes.get(genes.size() - 1).getValue());
 		izq = Converter.DoubleToInt(genes.get(genes.size() - 2).getValue());
 		value = (FunctionTSP.getDist(act, izq) + FunctionTSP.getDist(act, 25)) / 2 + 0.0; 
 		valores.add(value);
 		
-		// Hallamos los dos peores indices
+		// Hallamos los indices de los dos peores valores
 		int worst_index = 0;
 		int less_worse_index = 1;
 		if (valores.get(less_worse_index) > valores.get(worst_index)) {
 			worst_index = 1;
 			less_worse_index = 0;
-		} 
+		}
 		for (int i = 2; i < valores.size(); i++) {
-			if (valores.get(i) < valores.get(less_worse_index)) {
-				if (valores.get(i) < valores.get(worst_index)) {
+			if (valores.get(i) > valores.get(less_worse_index)) {
+				if (valores.get(i) > valores.get(worst_index)) {
 					less_worse_index = worst_index;
 					worst_index = i;
 				}
@@ -65,12 +69,12 @@ public class CityEugenicMutation implements CityMutationI {
 			}
 		}
 		
-		// Los intercambiamos de posicion
-		GenI worst = genes.get(worst_index).copy();
-		GenI less_worse = genes.get(less_worse_index).copy();
-		Double aux = worst.getValue();
+		// Intercambiamos de posición a los genes correspondientes.
+		GenI worst = genes.get(worst_index);
+		GenI less_worse = genes.get(less_worse_index);
+		Double worstValue = worst.getValue();
 		worst.assimilate(less_worse.getValue());
-		less_worse.assimilate(aux);
+		less_worse.assimilate(worstValue);
 		
 		return genes;
 	}
