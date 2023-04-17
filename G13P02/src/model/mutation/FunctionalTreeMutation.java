506546946c17363e42d6ce5e7e6d29e51ed21fcf
rafalonsog <rafas1367@gmail.com>
2023-04-17 09:41:37 +0200

    Mutacion por Contraccion y permutacion

---
+++ G13P02/src/model/mutation/FunctionalTreeMutation.java
@@ -0,0 +1,30 @@
+package model.mutation;
+
+import model.chromosome.TreeChromosome;
+import model.random.RandomGenerator;
+import model.tree.ArithmeticEnum;
+import model.tree.ArithmeticNode;
+import model.tree.TerminalEnum;
+
+public class FunctionalTreeMutation implements TreeMutationI {
+	
+	private double probability;
+	
+	public FunctionalTreeMutation(double probability) {
+		this.probability = probability;
+	}
+
+	@Override
+	public ArithmeticNode act(TreeChromosome treeChromosome) {
+		ArithmeticNode node = treeChromosome.getRaiz().copy();
+		if (RandomGenerator.createAleatoryBoolean(probability)) {
+			int index = RandomGenerator.createAleatoryInt(node.getNumSons());
+			if (node.getNode(index).isLeaf()) {
+				node.getNode(index).setFruit(TerminalEnum.values()[RandomGenerator.createAleatoryInt(TerminalEnum.values().length)]);
+			}
+			else node.getNode(index).setKnot(ArithmeticEnum.values()[RandomGenerator.createAleatoryInt(ArithmeticEnum.values().length)]);
+		}
+		return node;
+	}
+
+}
